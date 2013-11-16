6cf42d661facf1c11de545959b91c073709eac8e
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-11-16 00:16:30 +0000

    Add a test for extracting service domain from a link

--- webapp_tests.py
+++ webapp_tests.py
@@ -13,9 +13,23 @@ class WebappTestExtractingServiceDomainsFromLinks(unittest.TestCase):
         assert True == status
         assert "foo.service.gov.uk" == domain
 
+
     def test_extract_nonservice_domain_from_link(self):
         status, domain = webapp.extract_service_domain_from_link('https://foo.foo.gov.uk/blah')
         assert False == status
+
+
+class WebappTestExtractingServiceLinkFromSlug(unittest.TestCase):
+
+    def test_find_link_from_slug(self):
+        status, link = webapp.find_link_from_slug('/lasting-power-of-attorney')
+        assert True == status
+        assert "https://lastingpowerofattorney.service.gov.uk/" == link
+
+    def test_fail_to_find_link_from_slug(self):
+        status, link = webapp.find_link_from_slug('/bank-holidays')
+        assert False == status
+
 
 if __name__ == '__main__':
     unittest.main()
